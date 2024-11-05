09c536e9135443a826c40da05eba8eee267814ac
Hlib3 <Hlib3@users.noreply.github.com>
2024-11-06 01:54:45 +0200

    Einfügt aufgabe 1.

---
+++ Aufgabe_1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    std::cout << "String1\n\tString2\n\t\t String3" << endl;
+    cout << "\tString1\nString2 " << endl;
+    cout << 3;
+    cout << '*';
+    cout << "\tString\n1";
+}
+
+    //std::cout << showpos << '\t' << 4 << noshowpos << endl;
+    //cout << setw(17) << internal << fixed << setprecision(2) << -67.09124 << endl;
+    //cout << setw(10) << right << showpos << 235 << noshowpos << endl;
+    //cout << "8a1" << endl;
+    //cout << fixed << setprecision(2) << -121.0 << endl;
+    //cout << showpos << 1;
+    //cout << 24 << noshowpos << endl;
