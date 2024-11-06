bf2b8898c74c44cc678cf338d9d69f386f9c60b8
Hlib3 <Hlib3@users.noreply.github.com>
2024-11-06 02:09:16 +0200

    1 und 2 Aufgaben war geteilt und in 2 Files geschpeichert.

---
+++ Aufgabe_2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    std::cout << "String1\n\tString2\n\t\t String3" << endl;
+    cout << "\tString1\nString2 " << endl;
+    cout << 3;
+    cout << '*';
+    cout << "\tString\n1";
+}
